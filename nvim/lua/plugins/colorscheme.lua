0b54805201ed18a02b6184ec5a35568493c2eeaf
Pablo Henrique <pabllobeg@gmail.com>
2023-11-29 19:07:07 -0300

    Add TroubleNormal Highlight to colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,22 @@
 return {
+
 	{
 		"craftzdog/solarized-osaka.nvim",
 		lazy = true,
 		priority = 1000,
-		opts = function()
-			return {
-				transparent = true,
-			}
+		config = function()
+			-- Configure the solarized-osaka theme
+			local theme = require("solarized-osaka")
+			theme.setup({
+				transparent = false, -- non-transparent background
+				on_highlights = function(hl, c)
+					-- Customize the TroubleNormal highlight group
+					hl.TroubleNormal = {
+						bg = c.bg, -- Assuming 'c' is the color table provided by the theme
+						fg = c.fg,
+					}
+				end,
+			})
 		end,
 	},
 }
